5d8098b8e4b85e9cb07fc1ce14a637313ab48220
juhanPark <iiiamst16@gmail.com>
2024-11-17 19:26:19 +0900

    refactor: spring security 관련 user 엔티티, user detail service 코드 리팩터링

--- src/main/java/org/example/testsecurity/service/CustomUserDetailsService.java
+++ src/main/java/org/example/testsecurity/service/CustomUserDetailsService.java
@@ -1,15 +1,10 @@
 package org.example.testsecurity.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.testsecurity.dto.CustomUserDetails;
-import org.example.testsecurity.entity.User;
 import org.example.testsecurity.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,11 +13,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> op_user = userRepository.findByUsername(username);
-        if(op_user.isPresent()) {
-            return new CustomUserDetails(op_user.get());
-        }
-        return null;
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()-> new IllegalArgumentException(username));
     }
 }
